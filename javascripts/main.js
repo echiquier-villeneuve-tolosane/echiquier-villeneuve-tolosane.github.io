f11c275f86f6468b8757377f7d59b630649f0087
ebrehault <ebrehault@gmail.com>
2014-01-13 20:54:16 +0100

    allow to display chessboard

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+    $('.chessboard').each(function(i, el) {
+        el.style.width="400px";
+        var fen = $(el).text();
+        var board = new ChessBoard(el, {
+          draggable: true,
+          pieceTheme: '/images/chesspieces/wikipedia/{piece}.png'
+        });
+        board.position(fen, true);
+    });
+
+});
