43a5d44a1ee0bc05aeef43a3564d5552fbda27ee
Olive007007 <Olive007007@users.noreply.github.com>
2017-04-12 22:26:57 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         });
         board.position(fen, true);
     });
-    $('blockquote2').each(function(i, el) {
+    $('blockquote').each(function(i, el) {
         var link = $('<a href="#">Solution</a>');
         link.click(function() {
             $(el).toggle();
